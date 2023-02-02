eaf351ee8989da8b5105c1801affd9500aaf7f0d
guillemaguilapalleja <guillem.aguila,palleja@estudiantat.upc.edu>
2023-02-02 17:41:29 +0100

    Added pre-commit

--- LEDsService.py
+++ LEDsService.py
@@ -8,7 +8,6 @@ import neopixel
 import RPi.GPIO as GPIO
 
 
-
 LEDSequenceOn = False
 
 
@@ -16,7 +15,7 @@ def led_sequence():
     global op_mode
     global LEDSequenceOn
 
-    if op_mode == 'simulation':
+    if op_mode == "simulation":
 
         while LEDSequenceOn:
             print("RED")
@@ -34,13 +33,13 @@ def led_sequence():
             pixels[0] = (255, 255, 0)
             time.sleep(1)
 
+
 def on_message(client, userdata, message):
     global LEDSequenceOn
     splited = message.topic.split("/")
     origin = splited[0]
     destination = splited[1]
     command = splited[2]
-
 
     if command == "startLEDsSequence":
         print("Start LED sequence")
@@ -61,85 +60,85 @@ def on_message(client, userdata, message):
         time.sleep(int(seconds))
         LEDSequenceOn = False
 
-    if op_mode == 'simulation':
-        if command == 'red':
-            print('RED')
+    if op_mode == "simulation":
+        if command == "red":
+            print("RED")
             time.sleep(5)
-            print ('CLEAR')
+            print("CLEAR")
 
-        if command == 'green':
-            print('GREEN')
+        if command == "green":
+            print("GREEN")
             time.sleep(5)
-            print('CLEAR')
+            print("CLEAR")
 
-        if command == 'blue':
-            print('BLUE')
+        if command == "blue":
+            print("BLUE")
             time.sleep(5)
-            print('CLEAR')
+            print("CLEAR")
 
-        if command == 'drop':
-            if op_mode == 'simulation':
-                print('DROP')
+        if command == "drop":
+            if op_mode == "simulation":
+                print("DROP")
 
-        if command == 'reset':
+        if command == "reset":
             p.ChangeDutyCycle(2.5)
 
-        if command == 'bluei':
-            print('BLUE')
-        if command == 'redi':
-            print('RED')
-        if command == 'greeni':
-            print('GREEN')
-        if command == 'yellowi':
-            print('YELLOW')
-        if command == 'pinki':
-            print('PINK')
-        if command == 'whitei':
-            print('WHITE')
-        if command == 'blacki':
-            print('BLACK')
-        if command == 'clear':
-            print('CLEAR')
+        if command == "bluei":
+            print("BLUE")
+        if command == "redi":
+            print("RED")
+        if command == "greeni":
+            print("GREEN")
+        if command == "yellowi":
+            print("YELLOW")
+        if command == "pinki":
+            print("PINK")
+        if command == "whitei":
+            print("WHITE")
+        if command == "blacki":
+            print("BLACK")
+        if command == "clear":
+            print("CLEAR")
     else:
-        if command == 'red':
+        if command == "red":
             pixels[0] = (255, 0, 0)
             time.sleep(5)
             pixels[0] = (0, 0, 0)
 
-        if command == 'green':
+        if command == "green":
             pixels[1] = (0, 255, 0)
             time.sleep(5)
             pixels[1] = (0, 0, 0)
 
-        if command == 'blue':
+        if command == "blue":
             pixels[2] = (0, 0, 255)
             time.sleep(5)
             pixels[2] = (0, 0, 0)
 
-        if command == 'drop':
+        if command == "drop":
             p.ChangeDutyCycle(7.5)
 
-        if command == 'reset':
+        if command == "reset":
             p.ChangeDutyCycle(2.5)
-        if command == 'bluei':
+        if command == "bluei":
             pixels[0] = (0, 0, 255)
-        if command == 'redi':
+        if command == "redi":
             pixels[0] = (107, 0, 0)
-        if command == 'greeni':
+        if command == "greeni":
             pixels[0] = (0, 255, 0)
-        if command == 'yellowi':
+        if command == "yellowi":
             pixels[0] = (255, 255, 0)
-        if command == 'pinki':
+        if command == "pinki":
             pixels[0] = (255, 192, 203)
-        if command == 'whitei':
+        if command == "whitei":
             pixels[0] = (255, 255, 255)
-        if command == 'blacki':
+        if command == "blacki":
             pixels[0] = (10, 10, 10)
-        if command == 'clear':
+        if command == "clear":
             pixels[0] = (0, 0, 0)
 
 
-def LEDsService (connection_mode, operation_mode, external_broker, username, password):
+def LEDsService(connection_mode, operation_mode, external_broker, username, password):
 
     global op_mode
     global external_client
@@ -147,10 +146,9 @@ def LEDsService (connection_mode, operation_mode, external_broker, username, pas
     global pixels
     global p
 
-    print ('Connection mode: ', connection_mode)
-    print ('operation mode: ', operation_mode)
+    print("Connection mode: ", connection_mode)
+    print("operation mode: ", operation_mode)
     op_mode = operation_mode
-
 
     pixels = neopixel.NeoPixel(board.D18, 5)
     servoPIN = 17
@@ -163,27 +161,21 @@ def LEDsService (connection_mode, operation_mode, external_broker, username, pas
     internal_broker_address = "localhost"
     internal_broker_port = 1884
 
-
-
-    if connection_mode == 'global':
+    if connection_mode == "global":
         external_broker_address = external_broker
     else:
-        external_broker_address = 'localhost'
+        external_broker_address = "localhost"
 
-
-    print ('External broker: ', external_broker_address)
-
+    print("External broker: ", external_broker_address)
 
     # the external broker must run always in port 8000
     external_broker_port = 8000
 
-
     external_client = mqtt.Client("LEDs_external", transport="websockets")
-    if external_broker_address == 'classpip.upc.edu':
+    if external_broker_address == "classpip.upc.edu":
         external_client.username_pw_set(username, password)
     external_client.on_message = on_message
     external_client.connect(external_broker_address, external_broker_port)
-
 
     internal_client = mqtt.Client("LEDs_internal")
     internal_client.on_message = on_message
@@ -194,26 +186,24 @@ def LEDsService (connection_mode, operation_mode, external_broker, username, pas
     internal_client.subscribe("+/LEDsService/#")
     internal_client.loop_start()
     external_client.loop_start()
-    if operation_mode == 'simulation':
+    if operation_mode == "simulation":
         external_client.loop_forever()
     else:
         external_client.loop_start()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
-    connection_mode = sys.argv[1] # global or local
-    operation_mode = sys.argv[2] # simulation or production
+    connection_mode = sys.argv[1]  # global or local
+    operation_mode = sys.argv[2]  # simulation or production
     username = None
     password = None
-    if connection_mode == 'global':
+    if connection_mode == "global":
         external_broker = sys.argv[3]
-        if external_broker == 'classpip.upc.edu':
+        if external_broker == "classpip.upc.edu":
             username = sys.argv[4]
             password = sys.argv[5]
     else:
         external_broker = None
     LEDsService(connection_mode, operation_mode, external_broker, username, password)
-
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# LEDs service  
+# LEDs service
 
 ## Introduction
 
-The LEDs service is an on-board module that controls the LEDs and the servo installed in the drone platform, as required by the rest of modules in the Drone Engineering Ecosystem.   
+The LEDs service is an on-board module that controls the LEDs and the servo installed in the drone platform, as required by the rest of modules in the Drone Engineering Ecosystem.
 Dashboard or mobile applications will requiere the LEDs service to light certain LED with a given RGB color of to move the servo to drop and object.
 
 ## Installations
-In order to run and contribute you must install Python 3.7. We recommend PyCharm as IDE for development.    
+In order to run and contribute you must install Python 3.7. We recommend PyCharm as IDE for development.
 Contributions must follow the contribution protocol that you will find in the main repo of the Drone Engineering Ecosystem.
 [![DroneEngineeringEcosystem Badge](https://img.shields.io/badge/DEE-MainRepo-brightgreen.svg)](https://github.com/dronsEETAC/DroneEngineeringEcosystemDEE)
 
 
 ## Operation modes
-Althoug it may not have much sense, the LEDs service can be run in simulation mode. In this case the service just print in console the actions that would do in poduction mode. This can be useful for debug purposes. Clone the repo in your computer and install de requirements. Be also sure that you have running the internal broker at "localhost:1884". To run the service you must edit the run/debug configuration in PyCharm in order to pass the required arguments to the script. At least two parameters are required: connection_mode (global or local) and operation_mode (simulation or production). In case of global communication mode, a third parameter is requiered indicating the external broker to be used. In case the external broker requieres credentials, two additional parameters must be includes (username and password).   
-   
-The run the LEDs service in production mode you will need the boot.py script that you will find in the main repo of the Drone Engineering Ecosystem. Follow the instruction that you will find in that repo.   
+Althoug it may not have much sense, the LEDs service can be run in simulation mode. In this case the service just print in console the actions that would do in poduction mode. This can be useful for debug purposes. Clone the repo in your computer and install de requirements. Be also sure that you have running the internal broker at "localhost:1884". To run the service you must edit the run/debug configuration in PyCharm in order to pass the required arguments to the script. At least two parameters are required: connection_mode (global or local) and operation_mode (simulation or production). In case of global communication mode, a third parameter is requiered indicating the external broker to be used. In case the external broker requieres credentials, two additional parameters must be includes (username and password).
+
+The run the LEDs service in production mode you will need the boot.py script that you will find in the main repo of the Drone Engineering Ecosystem. Follow the instruction that you will find in that repo.
 [![DroneEngineeringEcosystem Badge](https://img.shields.io/badge/DEE-MainRepo-brightgreen.svg)](https://github.com/dronsEETAC/DroneEngineeringEcosystemDEE)
 
 ## Commands
@@ -22,28 +22,25 @@ In order to send a command to the LEDs service, a module must publish a message 
 ```
 "XXX/LEDsService/YYY"
 ```
-where XXX is the name of the module requiring the service and YYY is the name of the service that is required. Oviously, some of the commands include data that must be includes in the payload of the message to be published. 
+where XXX is the name of the module requiring the service and YYY is the name of the service that is required. Oviously, some of the commands include data that must be includes in the payload of the message to be published.
 
-The table bellow indicates all the commands that are accepted by the LEDs service in the current version.   
+The table bellow indicates all the commands that are accepted by the LEDs service in the current version.
 
-Command | Description | Payload 
---- | --- | --- 
-*startLEDsSequence* | start a clyclic sequence: red, green, yellow | No 
-*stopLEDsSequence* | stops the sequence | No 
+Command | Description | Payload
+--- | --- | ---
+*startLEDsSequence* | start a clyclic sequence: red, green, yellow | No
+*stopLEDsSequence* | stops the sequence | No
 *LEDsSequenceForNSeconds* | runs the cyclic sequience during a certain number of seconds | the number of seconds as string
-*red* | put in red the first led for 5 seconds | No 
-*green* | put in green the second led for 5 seconds | No 
+*red* | put in red the first led for 5 seconds | No
+*green* | put in green the second led for 5 seconds | No
 *blue* | put in lue the third led for 5 seconds | No
 *drop* | move the servo to drop the object | No
 *reset* | move the servo to its initial position | No
-*bluei* | fix the first led to blue | No 
+*bluei* | fix the first led to blue | No
 *redi* | fix the first led to red | No
-*yellowi* | fix the first led to yellow | No 
+*yellowi* | fix the first led to yellow | No
 *greeni* | fix the first led to green | No
-*pinki* | fix the first led to pink | No 
+*pinki* | fix the first led to pink | No
 *whitei* | fix the first led to white | No
 *blacki* | fix the first led to black | No
-*clear* | clear the first led | No 
-
-
-
+*clear* | clear the first led | No
